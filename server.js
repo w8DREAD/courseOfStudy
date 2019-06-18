5aba05a3b59a1ad6177e6838ccf699e4fb5c77a4
w8DREAD <dreadgodik@gmail.com>
2019-06-18 11:23:37 +0300

    complete

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const   http = require('http'),
-        director = require('./model_IT/data');
+const http = require('http')
+const director = require('./model_IT/data')
 
-
-
-http.createServer(function(request, response){
-
-    response.end(`
+http.createServer(function (request, response) {
+  response.end(`
     Release projects -- ${JSON.stringify(director.company.completeProjects.length)}
     
     Hiring -- ${JSON.stringify(director.hiring.length)}
@@ -13,22 +10,6 @@ http.createServer(function(request, response){
     Firing -- ${JSON.stringify(director.firing.length)}
     
     FiringList -- ${JSON.stringify(director.firing)}
-    
-
-    
-    Work.Web.list -- ${JSON.stringify(director.company.web.workers)}
-    
-    Work.Mobile.list -- ${JSON.stringify(director.company.mobile.workers)}
-    
-    Work.QA.list -- ${JSON.stringify(director.company.QA.workers)}
-    
-    Process.Web.list -- ${JSON.stringify(director.company.web.projectInProcess)}
-    
-    Process.Mobile.list -- ${JSON.stringify(director.company.mobile.projectInProcess)}
-    
-    Process.QA.list -- ${JSON.stringify(director.company.QA.projectInProcess)}
-    
-    Hiring.list -- ${JSON.stringify(director.hiring)}`);
-
-
-}).listen(3000);
+      
+    Hiring.list -- ${JSON.stringify(director.hiring)}`)
+}).listen(3000)
