83b2083d661a1a4cdae684cf219eb597113e1b26
dreadw8 <47567070+dreadw8@users.noreply.github.com>
2019-06-24 21:49:10 +0300

    commit

--- Async/Callbacks/server.js
+++ Async/Callbacks/server.js
@@ -6,18 +6,14 @@ const app = express()
 
 app.engine('html', cons.mustache)
 app.set('view engine', 'html')
-let obj
 
-  fs.readFile('data.json', 'utf8', function (err, data) {
-    if (err) throw err // we'll not consider error handling for now
-    obj = data
-  })
+let qwe = JSON.parse(fs.readFileSync('data.json', 'utf8'))
 
 app.get('/', function (req, res) {
-  res.render('template', obj)
-
-function createhtml (dataPath, templatePath, outputPath, callback) {
-  let qwe = JSON.parse(fs.readFileSync('data.json', 'utf8'))
-
-}
+  res.render('template', qwe)
+})
+// function createhtml (dataPath, templatePath, outputPath, callback) {
+//   let qwe = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+//
+// }
 app.listen(3000)
