3afffd7dba61f527ef8b6ce28747ada8b55d6b52
w8DREAD <dreadgodik@gmail.com>
2019-06-25 09:25:46 +0300

    commit

--- Async/Callbacks/server.js
+++ Async/Callbacks/server.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const express = require('express')
 const cons = require('consolidate')
+const mustache = require('mustache')
 
 const app = express()
 
@@ -9,11 +10,22 @@ app.set('view engine', 'html')
 
 let qwe = JSON.parse(fs.readFileSync('data.json', 'utf8'))
 
+// fs.writeFile('build.html', mustache.render(template, qwe), function (error) {
+//   if (error) throw error // если возникла ошибка
+//   console.log('Асинхронная запись файла завершена. Содержимое файла:')
+//   let data = fs.readFileSync('build.html', 'utf8')
+//   console.log(data) // выводим считанные данные
+// })
+
 app.get('/', function (req, res) {
   res.render('template', qwe)
 })
 // function createhtml (dataPath, templatePath, outputPath, callback) {
-//   let qwe = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+//   let qwe = JSON.parse(fs.readFileSync(dataPath, 'utf8'))
+// app.get('/', function (req, res) {
+//     res.render('templatePath', qwe)
+// })
 //
 // }
+
 app.listen(3000)
