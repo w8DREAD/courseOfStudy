03b296c6b76dccb0b960376332b44351342c796b
w8DREAD <dreadgodik@gmail.com>
2019-06-26 09:46:21 +0300

    push

--- Async/Callbacks/server.js
+++ Async/Callbacks/server.js
@@ -8,42 +8,20 @@ const app = express()
 app.engine('html', cons.mustache)
 app.set('view engine', 'html')
 
-let template
-let json
-
+// Callback
 function createhtml (dataPath, templatePath, outputPath, callback) {
-
+  let template
+  let json
   fs.readFile(dataPath, 'utf8', (err, data) => {
-    json =  JSON.parse(data)
+    json = JSON.parse(data)
+    fs.readFile(templatePath, 'utf8', (err, data) => {
+      template = data
+      callback(json, template, outputPath)
+    })
   })
-  fs.readFile(templatePath, 'utf8', (err, data) => {
-    template = data
-  })
-  callback(json, template, outputPath)
-  //
-  // let result = mustache.render(template, obj)
-  //
-  // fs.writeFile(outputPath, result, (err) => {
-  //   if (err) {
-  //     return console.log(err)
-  //   }
-  //   console.log('The file was saved!')
-  // })
 }
-// let obj
-// let template
-// fs.readFile('views/template.html', 'utf8', (err, data) => {
-//   template = data
-// })
-// fs.readFile('data.json', 'utf8', (err, data) => {
-//   obj = data
-// })
-//
-// let result = mustache.render(template, obj)
-//
 
 function callback (data, temp, outputPath) {
-  console.log(data)
   let result = mustache.render(temp, data)
   fs.writeFile(outputPath, result, (err) => {
     if (err) {
